d35a70df06cad557d27ccf7b8748dfa87ca009ec
Saeed Zare <saeed1381@gmail.com>
2024-07-17 12:20:15 +0330

    download mimetype bug fix

--- Project1-ConcurrentFileServer/main.go
+++ Project1-ConcurrentFileServer/main.go
@@ -2,72 +2,106 @@ package main
 
 import (
 	"ConcurrentFileServer/core"
-	"context"
+	"ConcurrentFileServer/utils"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"path/filepath"
 )
 
+type Error struct {
+	Error string `json:"error"`
+}
+
+type Success struct {
+	Message string `json:"message"`
+}
+
+type PostFile struct {
+	FileId string `json:"file_id"`
+}
+
 func downloadFile(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	handler := core.NewFileHandlerImpl()
+	fileId := r.PostFormValue("file_id")
+	if len(fileId) == 0 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		marshaled, _ := json.Marshal(Error{"file_id is empty"})
+		w.Write(marshaled)
 		return
 	}
 
-	handler := core.NewFileHandlerImpl()
-	ctx := context.Background()
-	fileId := r.PostFormValue("file_id")
+	file, _, err := handler.DownloadFile(r.Context(), fileId)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		marshaled, _ := json.Marshal(Error{err.Error()})
+		w.Write(marshaled)
+		return
+	}
 
-	fmt.Println("fileId:", fileId)
-	file, _, err := handler.DownloadFile(ctx, fileId)
-	fmt.Println(err)
-	fmt.Println(file)
-	w.Write(file)
+	filePath := filepath.Join("./files", fileId)
+	w.Header().Set("Content-Disposition", "attachment; filename="+filePath)
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Header().Set("Content-Length", string(rune(len(file))))
+
+	// Send the file content to the response
+	http.ServeFile(w, r, filePath)
+	//fmt.Println(file)
+	//w.Write(file)
 }
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
 	handler := core.NewFileHandlerImpl()
-	ctx := context.Background()
-	file, header, _ := r.FormFile("file")
-	fmt.Println(header)
-	fmt.Println(file)
+	file, fileHeader, err := r.FormFile("file")
 
-	fileData, err := ioutil.ReadAll(file)
-	fileId, err := handler.UploadFile(ctx, fileData, "text/plain")
-	fmt.Println(err)
-
-	data := struct {
-		FileId string `json:"file_id"`
-	}{
-		FileId: fileId,
-	}
-
-	err = json.NewEncoder(w).Encode(data)
 	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		marshaled, _ := json.Marshal(Error{err.Error()})
+		w.Write(marshaled)
 		return
 	}
+	extension := filepath.Ext(fileHeader.Filename)[1:]
+
+	fileData, err := io.ReadAll(file)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		marshaled, _ := json.Marshal(Error{err.Error()})
+		w.Write(marshaled)
+		return
+	}
+
+	fileId, err := handler.UploadFile(r.Context(), fileData, utils.GetMineTypeByExtension(extension))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		marshaled, _ := json.Marshal(Error{err.Error()})
+		w.Write(marshaled)
+		return
+	}
+
+	marshaledFileId, _ := json.Marshal(PostFile{fileId})
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(marshaledFileId)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))
+	w.Header().Set("Content-Type", "application/json")
+	jsonResponse, _ := json.Marshal(Success{Message: "Welcome to Upload/Download Server"})
+	w.Write(jsonResponse)
 }
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/downloadFile", downloadFile)
-	mux.HandleFunc("/uploadFile", uploadFile)
-	mux.HandleFunc("/", hello)
+	mux.HandleFunc("POST /downloadFile", downloadFile)
+	mux.HandleFunc("POST /uploadFile", uploadFile)
+	mux.HandleFunc("GET /", hello)
 
-	fmt.Printf("Server is running on 127.0.0.1:8080")
+	fmt.Println("Server is running on http://127.0.0.1:8080")
 
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		fmt.Println("Server error:", err)
